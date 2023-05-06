9b4b181d023a9913c8e6669767eec56e295808e7
Prakhar <prakharkumarsrivastav@gmail.com>
2023-05-06 20:12:39 +0530

    path change

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const ejs = require('ejs');
+const path = require('path');
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
   res.render('index');
