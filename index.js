45b7cb1452e58ef165dba93eda4ead6abc30ad1d
Prakhar <prakharkumarsrivastav@gmail.com>
2023-05-06 19:43:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
