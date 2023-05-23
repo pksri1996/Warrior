2a4d5a306f546fe81cb265fe2b3c843e7a0495b4
Prakhar <prakharkumarsrivastav@gmail.com>
2023-05-23 12:08:38 +0530

    new avatar

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
-const ejs = require('ejs');
 const path = require('path');
+const methodOverride = require('method-override');
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
